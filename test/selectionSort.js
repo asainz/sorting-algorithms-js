ac922b5be58cf0de40058ad249a9795c4abd3e2c
Andres Sainz de Aja <andressainz@gmail.com>
2013-08-02 21:21:27 -0300

    improve tests readability

--- test/selectionSort.js
+++ test/selectionSort.js
@@ -2,15 +2,26 @@
 
 describe('Selection Sort', function () {
 
+    var sort;
+
+    beforeEach(function(){
+        sort = selectionSort.sort;
+    });
+
     it('should return undefined if the parameter is not an array ', function () {
-        expect( selectionSort.sort('hello') ).toBeUndefined();
-        expect( selectionSort.sort(1) ).toBeUndefined();
-        expect( selectionSort.sort({}) ).toBeUndefined();
-        expect( selectionSort.sort() ).toBeUndefined();
+        expect( sort('hello') ).toBeUndefined();
+        expect( sort(1) ).toBeUndefined();
+        expect( sort({}) ).toBeUndefined();
+        expect( sort() ).toBeUndefined();
     });
 
     it('should sort an unsorted array in ascendent way', function(){
-        expect( selectionSort.sort([5,1,6,2,9,4]) ).toEqual( [1,2,4,5,6,9] );
+        expect( sort([5,1,6,2,9,4]) ).toEqual( [1,2,4,5,6,9] );
+    });
+
+    it('should return the array untouched if it\'s already sorted', function(){
+        var param = [1,2,3,4,5,6];
+        expect( sort( param ) ).toEqual(param);
     });
 
 });
